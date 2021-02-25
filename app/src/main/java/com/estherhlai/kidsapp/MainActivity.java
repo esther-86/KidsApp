eb9c1794386ce533143673d82a137f4fcd0df485
esther-86 <mysti9uemirage@yahoo.com>
2021-02-24 16:23:54 -0800

    Floating widget service, from https://www.youtube.com/watch?v=UwnhGkahoao

--- app/src/main/java/com/estherhlai/kidsapp/MainActivity.java
+++ app/src/main/java/com/estherhlai/kidsapp/MainActivity.java
@@ -1,14 +1,51 @@
 package com.estherhlai.kidsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int DRAW_OVER_OTHER_APP_PERMISSION = 123;
+    private Button buttonAddWidget;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        askForSystemOverlayPermission();
+
+        buttonAddWidget = (Button) findViewById(R.id.button);
+        buttonAddWidget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!Settings.canDrawOverlays(MainActivity.this)) {
+                    askForSystemOverlayPermission();
+                } else {
+                    Intent intent = new Intent(MainActivity.this, FloatingWidgetService.class);
+                    startService(intent);
+                    finish();
+                }
+            }
+        });
     }
+
+    private void askForSystemOverlayPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            //If the draw over permission is not available open the settings screen
+            //to grant the permission.
+            Intent intent = new Intent(
+                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, DRAW_OVER_OTHER_APP_PERMISSION);
+        }
+    }
+
 }
